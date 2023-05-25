f9cbb8f6f8e652dbc1b51d2d9e23fec51226fb36
yusong17 <songyuhust+1@gmail.com>
2023-05-24 20:55:35 -0700

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+module.exports = {
+  pathPrefix: process.env.PATH_PREFIX || '/',
+  siteMetadata: {
+    siteTitle: 'Run Yu Run',
+    siteUrl: 'https://yusong17.github.io/running',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    description: 'Yu is running',
+    navLinks: [
+      {
+        name: 'Yu Song',
+        url: 'https://yusong17.github.io',
+      },
+      {
+        name: 'Follow me',
+        url: 'https://strava.app.link/UaFKv6EtWzb',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+
+    {
+      resolve: 'gatsby-plugin-vercel',
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: '.',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+  ],
+};
